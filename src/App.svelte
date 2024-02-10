34487374e310b0ec0c96f9e38677a33101802264
j-sabile <jericosabile@gmail.com>
2024-02-11 02:41:38 +0800

    add average of and best of

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,11 @@
   let status = "none";
   let tempTimeout;
   let tempInterval;
+  let times = [];
+  let currAO5;
+  let currAO12;
+  let bestAO5;
+  let bestAO12;
 
   const handleKeyDown = (e) => {
     if (e.code !== "Space" || status === "inspect" || status === "inspect-done") return;
@@ -14,6 +19,15 @@
     }
     if (status === "solving") {
       clearInterval(tempInterval);
+      times = [...times, Number(time.toFixed(2))];
+      if (times.length >= 5) {
+        currAO5 = (times.slice(-5).reduce((acc, cur) => acc + cur, 0) / 5).toFixed(2);
+        if (currAO5 < bestAO5 || !bestAO5) bestAO5 = currAO5;
+      }
+      if (times.length >= 12) {
+        currAO12 = (times.slice(-12).reduce((acc, cur) => acc + cur, 0) / 12).toFixed(2);
+        if (currAO12 < bestAO12 || !bestAO12) bestAO12 = currAO12;
+      }
       status = "none";
     }
   };
@@ -31,8 +45,26 @@
   };
 </script>
 
-<main class="stat-{status} h-[100dvh] flex justify-center items-center">
+<main class="stat-{status} h-[100dvh] w-[100dvw] flex justify-center items-center">
   <h1 class="text-[6rem] font-mono">{time.toFixed(2)}</h1>
+
+  <table class="absolute bottom-0 left-0 text-xl m-4">
+    <tr>
+      <td />
+      <td class="text-center px-2">Current</td>
+      <td class="text-center px-2">Best</td>
+    </tr>
+    <tr>
+      <td class="text-center">AO5</td>
+      <td class="text-center">{currAO5 ? currAO5 : "-"}</td>
+      <td class="text-center">{bestAO5 ? bestAO5 : "-"}</td>
+    </tr>
+    <tr>
+      <td class="text-center">AO12</td>
+      <td class="text-center">{currAO12 ? currAO12 : "-"}</td>
+      <td class="text-center">{bestAO12 ? bestAO12 : "-"}</td>
+    </tr>
+  </table>
 </main>
 
 <svelte:window on:keydown={handleKeyDown} on:keyup={handleKeyUp} />
